7433fa4dcd846719fae3db35d19809269f153824
Aditya Prakash <prakash.adi@northeastern.edu>
2023-10-04 02:07:00 -0400

    initial push for assignment 03

---
+++ main.py
@@ -0,0 +1,184 @@
+from app import app
+from flask import request
+import service
+from config import set_db_creds
+from bootstrap import engine
+
+# handles the API requests for GET method
+
+
+@app.route('/healthz', methods=['GET'])
+def health_check():
+
+    if (request.data or request.args):
+        print("Method Not Allowed")
+        return service.prepare_response(400)
+    else:
+        set_db_creds()
+        if service.check_database_connection():
+            print("Success")
+            return service.prepare_response(200)
+        else:
+            print("Service Unavailable")
+            return service.prepare_response(503)
+
+
+# handles the restricted methods for the API
+@app.route('/healthz', methods=['POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'])
+def handle_methods():
+    print("Method Not Allowed")
+    return service.prepare_response(405)
+
+
+# handles the error when the requested page is not available
+@app.errorhandler(404)
+def showMessage(error=None):
+    print("Not Found")
+    return service.prepare_response(404)
+
+
+# handles the error when service is unavailable
+@app.errorhandler(500)
+def showMessage(error=None):
+    print("Service Unavailable")
+    return service.prepare_response(503)
+
+
+# handles assignment creation
+@app.route('/v1/assignments', methods=['POST'])
+def handle_create_assignment():
+    print("inside post method")
+    print(request.headers.get('Authorization'))
+    try:
+        _, base64_encoded_credentials = request.headers.get(
+            'Authorization').split(" ", 1)
+        print(base64_encoded_credentials)
+    except ValueError:
+        print("Invalid Authorization header format")
+        base64_encoded_credentials = None
+
+    if (service.check_creds(base64_encoded_credentials)):
+        request_data = request.get_json()
+        mandatory_fields = ['name', 'points', 'num_of_attempts', 'deadline']
+
+        if not service.validate_mandatory_fields(request_data, mandatory_fields):
+            return service.prepare_response(400)
+
+        assignment_data = service.create_assignment(
+            base64_encoded_credentials, request_data)
+
+        return service.prepare_assignments_response(201, assignment_data)
+    else:
+        return service.prepare_response(401)
+
+
+# handles assignment updates
+@app.route('/v1/assignments/<assignment_id>', methods=['PUT'])
+def handle_update_assignment(assignment_id):
+    print("inside put method")
+    print(request.headers.get('Authorization'))
+    try:
+        _, base64_encoded_credentials = request.headers.get(
+            'Authorization').split(" ", 1)
+        print(base64_encoded_credentials)
+    except ValueError:
+        print("Invalid Authorization header format")
+        base64_encoded_credentials = None
+
+    if (service.check_creds(base64_encoded_credentials)):
+        if(service.check_owner(assignment_id, base64_encoded_credentials)):
+            
+            request_data = request.get_json()
+            mandatory_fields = ['name', 'points', 'num_of_attempts', 'deadline']
+
+            if not service.validate_mandatory_fields(request_data, mandatory_fields):
+                return service.prepare_response(400)
+            
+            service.update_assignment(assignment_id, request.get_json())
+            return service.prepare_response(204)
+        else:
+            return service.prepare_response(403)
+    else:
+        return service.prepare_response(401)
+
+
+# handles delete assignments
+@app.route('/v1/assignments/<assignment_id>', methods=['DELETE'])
+def handle_delete_assignment(assignment_id):
+    print("inside delete method")
+    print(request.headers.get('Authorization'))
+    try:
+        _, base64_encoded_credentials = request.headers.get(
+            'Authorization').split(" ", 1)
+        print(base64_encoded_credentials)
+    except ValueError:
+        print("Invalid Authorization header format")
+        base64_encoded_credentials = None
+
+    if (service.check_creds(base64_encoded_credentials)):
+        if(service.check_owner(assignment_id, base64_encoded_credentials)):
+            if(service.delete_assignment(assignment_id)):
+                return service.prepare_response(204)
+        else:
+            return service.prepare_response(403)
+    else:
+        return service.prepare_response(401)
+
+
+# handles get all assignments
+@app.route('/v1/assignments', methods=['GET'])
+def handle_get_all_assignments():
+
+    if (request.data or request.args):
+        print("Method Not Allowed")
+        return service.prepare_response(400)
+    else:
+        try:
+            _, base64_encoded_credentials = request.headers.get(
+                'Authorization').split(" ", 1)
+            print(base64_encoded_credentials)
+        except ValueError:
+            print("Invalid Authorization header format")
+            base64_encoded_credentials = None
+
+        if (service.check_creds(base64_encoded_credentials)):
+            print("inside get all assignments method")
+            assignment_list = service.get_all_assignment()
+            return service.prepare_assignments_response(200, assignment_list)
+        else:
+            return service.prepare_response(401)
+
+
+@app.route('/v1/assignments/<assignment_id>', methods=['GET'])
+def handle_get_by_id_assignment(assignment_id):
+    print("inside get by id method")
+    print(request.headers.get('Authorization'))
+    try:
+        _, base64_encoded_credentials = request.headers.get(
+            'Authorization').split(" ", 1)
+        print(base64_encoded_credentials)
+    except ValueError:
+        print("Invalid Authorization header format")
+        base64_encoded_credentials = None
+
+    if (service.check_creds(base64_encoded_credentials)):
+        if(service.check_owner(assignment_id, base64_encoded_credentials)):
+            assignment_data = service.get_assignment_by_id(assignment_id)
+            return service.prepare_assignments_response(200, assignment_data)
+        else:
+            return service.prepare_response(403)
+    else:
+        return service.prepare_response(401)
+
+
+# run test cases
+service.test_get_case()
+service.test_post_case()
+service.test_not_found_case()
+
+
+# main method
+if __name__ == "__main__":
+    with engine.connect() as connection:
+        connection.execute("SELECT 1")
+    app.run(host='127.0.0.1', port=5000)
---
+++ service.py
@@ -0,0 +1,140 @@
+from app import app
+from config import mysql
+from flask import jsonify
+import bootstrap
+import base64
+import bcrypt
+import uuid
+
+# function to check the database connectivity
+
+
+def check_database_connection():
+    try:
+        conn = mysql.connect()
+        conn.ping(reconnect=True)
+        print("Database connection successful")
+        return True
+    except Exception as e:
+        print("Database connection failed", e)
+        return False
+
+
+# fuction to prepare the response structure of the API
+def prepare_response(status_code):
+    response = jsonify()
+    response.data = b''
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    response.headers['Content-Type'] = 'application/json'
+    response.headers['Connection'] = 'keep-alive'
+
+    response.status_code = status_code
+
+    return response
+
+
+def check_creds(base64_encoded_credentials):
+    print("inside check_creds method")
+    decoded_credentials = base64.b64decode(
+        base64_encoded_credentials).decode("utf-8")
+    email, password = decoded_credentials.split(":")
+    encoded_password = bootstrap.fetch_password(email)
+    print(password.encode('utf-8'))
+    print(encoded_password)
+    if bcrypt.checkpw(password.encode('utf-8'), encoded_password):
+        return True
+    else:
+        return False
+
+
+def validate_mandatory_fields(request_data, mandatory_fields):
+    for field in mandatory_fields:
+        if field not in request_data:
+            return False
+    return True
+
+
+def create_assignment(request_auth, request_body):
+
+    print(request_auth, request_body)
+
+    if 1 <= request_body['points'] <= 10:
+        assignment = bootstrap.create_assignment_db(request_auth, request_body)
+        print(assignment)
+        return assignment
+    else:
+        return False
+
+
+def check_owner(assignment_id, base64_encoded_credentials):
+    owner = bootstrap.fetch_owner(assignment_id)
+    if owner == base64_encoded_credentials:
+        return True
+    else:
+        return False
+
+
+def update_assignment(assignment_id, request_body):
+    print(request_body)
+    if 1 <= request_body['points'] <= 10:
+        return bootstrap.update_assignment_db(assignment_id, request_body)
+
+
+def delete_assignment(assignment_id):
+    print(assignment_id)
+    if(bootstrap.delete_assignment_db(assignment_id)):
+        return True
+    else:
+        return False
+
+
+def get_all_assignment():
+    assigment_list = bootstrap.get_all_assignments_db()
+    print(assigment_list)
+    return assigment_list
+
+
+def get_assignment_by_id(assignment_id):
+    assignment = bootstrap.get_assignment_by_id_db(assignment_id)
+    print(assignment)
+    return assignment
+
+
+def prepare_assignments_response(status_code, assignment_list):
+    response = jsonify(assignment_list)
+    
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    response.headers['Content-Type'] = 'application/json;charset=utf-8'
+    response.headers['Connection'] = 'keep-alive'
+
+    # response.headers['Access-control-allow-credentials'] = 'true'
+    # response.headers['Access-control-allow-headers'] = 'X-Requested-With,Content-Type,Accept,Origin'
+    # response.headers['Access-control-allow-methods'] = 'true'
+    # response.headers['Access-control-allow-credentials'] = '*' 
+    # response.headers['Content-encoding'] = 'gzip'
+    # response.headers['X-firefox-spdy'] = 'h2'
+  
+    response.status_code = status_code
+    
+    return response
+###################### test cases ######################
+
+
+def test_get_case():
+    response_get = app.test_client().get('/healthz')
+    assert response_get.status_code == 200
+
+
+def test_post_case():
+    response_post = app.test_client().post('/healthz')
+    assert response_post.status_code == 405
+
+
+def test_not_found_case():
+    response_not_found = app.test_client().get('/healthz/')
+    assert response_not_found.status_code == 404
+###################### test cases ######################
