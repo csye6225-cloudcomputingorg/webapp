3dcc9f814711b4ef190b81584402ab149eecb95f
Aditya Prakash <prakash.adi@northeastern.edu>
2023-10-25 19:36:55 -0400

    Feature/assignment06 (#31)
    * Update bootstrap.py
    
    AWS RDS changes
    
    * Update config.py
    
    * Update installation_script.sh
    
    systemd related changes
    
    * Create webapp.service
    
    webapp.service systemd file
    
    * Update validate_packer.yaml

--- config.py
+++ config.py
@@ -4,16 +4,40 @@ from flaskext.mysql import MySQL
 from dotenv import load_dotenv
 
 # load environment variables
-load_dotenv()
+# load_dotenv()
+
+# Fetch user data
+user_data_url = "http://169.254.169.254/latest/user-data"
+response = requests.get(user_data_url)
+user_data = response.text
+
+# Split user data into lines
+lines = user_data.split('\n')
+
+# Parse environment variable assignments
+environment_variables = {}
+for line in lines:
+    if line.strip().startswith("export "):
+        parts = line.strip()[7:].split("=")
+        if len(parts) == 2:
+            key = parts[0].strip()  # Remove "export " prefix
+            value = parts[1].strip()
+            environment_variables[key] = value
+
+# Access the environment variables
+db_host = environment_variables.get("DB_HOST")
+db_name = environment_variables.get("DB_NAME")
+db_user = environment_variables.get("DB_USER")
+db_password = environment_variables.get("DB_PASSWORD")
 
 
 # fetch database credentials from environment variables
 def set_db_creds():
-    app.config['MYSQL_DATABASE_USER'] = os.getenv('db_user')
-    app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv('db_password')
-    #app.config['MYSQL_DATABASE_DB'] = os.getenv('db_name')
-    app.config['MYSQL_DATABASE_HOST'] = os.getenv('db_host')
-    print(os.getenv('db_user'))
+    app.config['MYSQL_DATABASE_USER'] = db_user
+    app.config['MYSQL_DATABASE_PASSWORD'] = db_password
+    #app.config['MYSQL_DATABASE_DB'] = db_name
+    app.config['MYSQL_DATABASE_HOST'] = db_host
+    # print(os.getenv('db_user'))
 
     #print(os.getenv('db_password'), os.getenv('db_user'), os.getenv('db_host'))
 
--- installation_script.sh
+++ installation_script.sh
@@ -4,13 +4,13 @@ echo_info () {
     echo $1
 }
 
+sudo groupadd csye6225
+sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
+
 echo_info "---------UPDATES-BEING-INSTALLED---------"
 
 # Update the system
 sudo apt update && sudo apt upgrade -y
-
-# # Install necessary packages
-# sudo apt install -y python3 python3-pip mariadb-server mariadb-devel unzip
 
 # Installing python server
 echo_info "---------INSTALLING-PYTHON---------"
@@ -25,14 +25,14 @@ source myenv/bin/activate
 pip3 install bcrypt==4.0.1 blinker==1.6.3 certifi==2023.7.22 charset-normalizer==3.3.0 click==8.1.7 Flask==2.2.5 Flask-Cors==4.0.0 Flask-MySQL==1.5.2 greenlet==3.0.0 idna==3.4 iniconfig==2.0.0 itsdangerous==2.1.2 Jinja2==3.1.2 MarkupSafe==2.1.3 mysql-connector-python==8.1.0 packaging==23.2 pip==23.0.1 pluggy==1.3.0 protobuf==4.21.12 PyMySQL==1.1.0 pytest==7.4.2 python-dotenv==1.0.0 requests==2.31.0 setuptools==66.1.1 SQLAlchemy==2.0.21 typing_extensions==4.8.0 urllib3==2.0.6 Werkzeug==3.0.0
 
 # Start and enable MariaDB service
-echo_info "---------INSTALLING-MARIADB---------"
-sudo apt install mariadb-server -y
-sudo systemctl start mariadb
-sudo systemctl enable mariadb
-sudo systemctl status mariadb
+# echo_info "---------INSTALLING-MARIADB---------"
+# sudo apt install mariadb-server -y
+# sudo systemctl start mariadb
+# sudo systemctl enable mariadb
+# sudo systemctl status mariadb
 
-sudo mysql -u root -ppassword -e 'CREATE DATABASE webapp;',
-sudo mysql -u root --skip-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';"
+# sudo mysql -u root -ppassword -e 'CREATE DATABASE webapp;',
+# sudo mysql -u root --skip-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';"
 
 # Secure MariaDB installation (optional but recommended)
 # Make sure to set a strong password for the root user
@@ -48,32 +48,10 @@ unzip webapp.zip -d webapp
 rm webapp.zip
 cd webapp || exit
 
-# python app.py  # Or whatever command starts your web app
+sudo cp webapp.service /etc/systemd/system/
 
-# Your web application should now be running
+sudo systemctl daemon-reload
+sudo systemctl enable webapp.service
+sudo systemctl start webapp.service
 
-# Optionally, create a systemd service to keep your app running on system restart
-# Write a systemd service file and enable it
-# For example:
-# sudo nano /etc/systemd/system/my-webapp.service
-# [Unit]
-# Description=My Web Application
-#
-# [Service]
-# ExecStart=/path/to/your/start_script.sh
-# WorkingDirectory=/path/to/your/web_app_directory
-# User=your_username
-# Restart=always
-#
-# [Install]
-# WantedBy=multi-user.target
-#
-# Then, enable and start the service:
-# sudo systemctl enable my-webapp
-# sudo systemctl start my-webapp
-
-# Don't forget to open necessary ports in your security group for your web app
-# (e.g., HTTP port 80, HTTPS port 443, etc.)
-
-# When you're done, deactivate the virtual environment
 deactivate
