24d5b45c09a0fc503c9dad67ccfd21b17d3eae49
Aditya Prakash <prakash.adi@northeastern.edu>
2023-11-06 02:44:02 -0500

    Feature/assignment07 (#49)
    * routes and auth changes
    
    * cloudwatch installation script
    
    * cloud-watch changes

---
+++ logging_config.py
@@ -0,0 +1,17 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+def setup_logging():
+    log_file = '/var/log/app.log'
+    log_handler = RotatingFileHandler(log_file, maxBytes=10240, backupCount=5)
+    log_format = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+
+    log_handler.setFormatter(log_format)
+    log_handler.setLevel(logging.DEBUG)
+
+    logger = logging.getLogger('my_app')
+    logger.addHandler(log_handler)
+    return logger
+
+logger = setup_logging()
+# logger.setLevel(logging.INFO)
--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from app import app
 from flask import request, redirect, Response
 import service
 # from config import set_db_creds
-from bootstrap import engine
+from logging_config import logger
+# from bootstrap import engine
 
 
 # handles the API requests redirection
 @app.route('/', methods=['GET'])
 def healthz_redirect():
+    logger.info("Re-directing to Assignments page")
     return redirect('/v1/assignments')
 
 # handles the API requests for GET method
@@ -15,50 +17,53 @@ def healthz_redirect():
 def health_check():
 
     if (request.data or request.args):
-        print("Method Not Allowed")
+        logger.error("Method Not Allowed (Request body, query params or path params not allowed)")
         return service.prepare_response(400)
     else:
      #       set_db_creds()
         if service.check_database_connection():
-            print("Success")
+            logger.info("Successful Database Connection")
             return service.prepare_response(200)
         else:
-            print("Service Unavailable")
+            logger.error("Service Unavailable (Database Connection Refused)")
             return service.prepare_response(503)
 
 
 # handles the restricted methods for the API
 @app.route('/healthz', methods=['POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'])
 def handle_methods():
-    print("Method Not Allowed")
+    logger.error("Method Not Allowed (Only GET method is allowed)")
     return service.prepare_response(405)
 
 
 # handles the error when the requested page is not available
 @app.errorhandler(404)
 def showMessage(error=None):
-    print("Not Found")
+    logger.error("Not Found (Please check the URL)")
     return service.prepare_response(404)
 
 
 # handles the error when service is unavailable
 @app.errorhandler(500)
 def showMessage(error=None):
-    print("Service Unavailable")
+    logger.error("Service Unavailable (Unexpected error)")
     return service.prepare_response(503)
 
 
 # handles assignment creation
 @app.route('/v1/assignments', methods=['POST'])
 def handle_create_assignment():
-    print("inside post method")
-    print(request.headers.get('Authorization'))
+    logger.info("Inside Create Assignment Method")
+    logger.debug("Authorisation Header: " + request.headers.get('Authorization'))
+
     try:
         auth = request.authorization
+        logger.debug("Auth Input: " + auth)
         if not auth:
+            logger.error("Unauthorised (Enter credentials)")
             return Response('Unauthorized', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
     except ValueError:
-        print("Invalid Authorization header format")
+        logger.error("Invalid Authorization header format")
         return service.prepare_response(400)
 
     if (service.check_creds(auth)):
@@ -66,30 +71,36 @@ def handle_create_assignment():
         mandatory_fields = ['name', 'points', 'num_of_attempts', 'deadline']
 
         if not service.validate_mandatory_fields(request_data, mandatory_fields):
+            logger.error("Mandatory fields missing")
             return service.prepare_response(400)
 
         assignment_data = service.create_assignment(
             auth, request_data)
 
         if (assignment_data):
+            logger.info("Assignment Created Successfully")
             return service.prepare_assignments_response(201, assignment_data)
         else:
+            logger.error("Bad Request, check values for points and num_of_attempts fields")
             return service.prepare_response(400)
     else:
+        logger.error("Unauthorised, check credentials")
         return service.prepare_response(401)
 
 
 # handles assignment updates
 @app.route('/v1/assignments/<assignment_id>', methods=['PUT'])
 def handle_update_assignment(assignment_id):
-    print("inside put method")
-    print(request.headers.get('Authorization'))
+    logger.info("Inside Update Assignment Method")
+    logger.debug("Authorisation Header: " + request.headers.get('Authorization'))
+    
     try:
         auth = request.authorization
         if not auth:
+            logger.error("Unauthorised, enter Auth values")
             return Response('Unauthorized', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
     except ValueError:
-        print("Invalid Authorization header format")
+        logger.error("Invalid Authorization header format")
         return service.prepare_response(400)
 
     if (service.check_creds(auth)):
@@ -100,92 +111,115 @@ def handle_update_assignment(assignment_id):
                                 'num_of_attempts', 'deadline']
 
             if not service.validate_mandatory_fields(request_data, mandatory_fields):
+                logger.error("Bad Request, Mandatory Fields Missing")
                 return service.prepare_response(400)
 
             assignment_data = service.update_assignment(
                 assignment_id, request.get_json())
             if assignment_data:
+                logger.info("Assignment Updated Successfully")
                 return service.prepare_response(204)
             else:
+                logger.error("Bad Request, check values for points and num_of_attempts fields")
                 return service.prepare_response(400)
         elif (service.check_owner(assignment_id, auth) == "No Match"):
+            logger.error("Forbidden, check credentials")
             return service.prepare_response(403)
         else:
+            logger.error("Assignment not found for update")
             return service.prepare_response(404)
     else:
+        logger.error("Unauthorised, check credentials")
         return service.prepare_response(401)
 
 
 # handles delete assignments
 @app.route('/v1/assignments/<assignment_id>', methods=['DELETE'])
 def handle_delete_assignment(assignment_id):
-    print("inside delete method")
-    print(request.headers.get('Authorization'))
+    logger.info("Inside Delete Assignment Method")
+    logger.debug("Authorisation Header: " + request.headers.get('Authorization'))
+    
     try:
         auth = request.authorization
         if not auth:
+            logger.error("Unauthorised, enter Auth values")
             return Response('Unauthorized', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
     except ValueError:
-        print("Invalid Authorization header format")
+        logger.error("Invalid Authorization header format")
         return service.prepare_response(400)
 
     if (service.check_creds(auth)):
         if (service.check_owner(assignment_id, auth) == "Match"):
             if (service.delete_assignment(assignment_id)):
+                logger.info("Assignment Deleted Successfully")
                 return service.prepare_response(204)
         elif (service.check_owner(assignment_id, auth) == "No Match"):
+            logger.error("Forbidden, check credentials")
             return service.prepare_response(403)
         else:
+            logger.error("Assignment not found for deletion")
             return service.prepare_response(404)
     else:
+        logger.error("Unauthorised, check credentials")
         return service.prepare_response(401)
 
 
 # handles get all assignments
 @app.route('/v1/assignments', methods=['GET'])
 def handle_get_all_assignments():
+    logger.info("Inside Get All Assignment Method")
+    logger.debug("Authorisation Header: " + request.headers.get('Authorization'))
 
     if (request.data or request.args):
-        print("Method Not Allowed")
+        logger.error("Method Not Allowed (Request body, query params or path params not allowed)")
         return service.prepare_response(400)
     else:
         try:
             auth = request.authorization
             if not auth:
+                logger.error("Unauthorised, enter Auth values")
                 return Response('Unauthorized', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
         except ValueError:
-            print("Invalid Authorization header format")
+            logger.error("Invalid Authorization header format")
             return service.prepare_response(400)
 
         if (service.check_creds(auth)):
-            print("inside get all assignments method")
+            # print("inside get all assignments method")
             assignment_list = service.get_all_assignment()
+            logger.info("Success All Assignments retrieved")
             return service.prepare_assignments_response(200, assignment_list)
         else:
+            logger.error("Unauthorised, check credentials")
             return service.prepare_response(401)
 
 
 @app.route('/v1/assignments/<assignment_id>', methods=['GET'])
 def handle_get_by_id_assignment(assignment_id):
-    print("inside get by id method")
-    print(request.headers.get('Authorization'))
+    logger.info("Inside Get Assignment by ID Method")
+    logger.debug("Authorisation Header: " + request.headers.get('Authorization'))
+    
     try:
         auth = request.authorization
         if not auth:
+            logger.error("Unauthorised, enter Auth values")
             return Response('Unauthorized', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
     except ValueError:
-        print("Invalid Authorization header format")
+        logger.error("Invalid Authorization header format")
         return service.prepare_response(400)
 
     if (service.check_creds(auth)):
         if (service.check_owner(assignment_id, auth) == "Match"):
             assignment_data = service.get_assignment_by_id(assignment_id)
+            logger.info("Assignment retrieved")
             return service.prepare_assignments_response(200, assignment_data)
         elif (service.check_owner(assignment_id, auth) == "No Match"):
+            logger.error("Forbidden, check credentials")
             return service.prepare_response(403)
         else:
+            logger.error("Assignnment not found")
             return service.prepare_response(404)
     else:
+        logger.error("Unauthorised, check credentials")
         return service.prepare_response(401)
 
 
