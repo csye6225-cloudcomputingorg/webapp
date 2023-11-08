367d5e6fadfcbe5c7812ecfb4dbde9b324c3ca8f
Aditya Prakash <prakash.adi@northeastern.edu>
2023-11-08 12:11:10 -0500

    Update statsd_config.py

--- statsd_config.py
+++ statsd_config.py
@@ -6,7 +6,7 @@ statsd_client = statsd.StatsClient('127.0.0.1', 8125)
 
 
 def handle_metric_count(status):
-    metric_name = "API: " + request.path + "API Method: " + request.method + status
+    metric_name = "API:" + request.path + "_API Method:" + request.method + "_" + status
     logger.info(f"metric incremented {metric_name}")
     print(metric_name)
     statsd_client.incr(metric_name, 1) 
