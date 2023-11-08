ad10c65c6f745a5a1694e88b2936640c422a28e3
Aditya Prakash <prakash.adi@northeastern.edu>
2023-11-08 17:41:15 -0500

    Feature/assignment07 (#61)
    * routes and auth changes
    
    * cloudwatch installation script
    
    * cloud-watch changes
    
    * changes in shell script
    
    * changes in shell script and config
    
    * Update .gitignore
    
    * Update .gitignore
    
    * changes in shell script and config for checking cloudwatch config
    
    * changes in shell script and config for working cloudwatch config
    
    * corrected path for logs
    
    * config json updated
    
    * dialog box and 403 changes
    
    * Update logging_config.py
    
    * Update installation_script.sh
    
    * changes for statsd metrics
    
    * Update statsd_config.py
    
    * Update installation_script.sh
    
    * Update cloudwatch-config.json
    
    * Update statsd_config.py
    
    * metrics host
    
    * metrics changes success
    
    * Update statsd_config.py

--- main.py
+++ main.py
@@ -17,16 +17,16 @@ def health_check():
 
     if (request.data or request.args):
         logger.error("Method Not Allowed (Request body, query params or path params not allowed)")
-        handle_metric_count("failed, 400") 
+        handle_metric_count("failed_400") 
         return service.prepare_response(400)
     else:
         if service.check_database_connection():
             logger.info("Successful Database Connection")
-            handle_metric_count("success, 200") 
+            handle_metric_count("success_200") 
             return service.prepare_response(200)
         else:
             logger.error("Service Unavailable (Database Connection Refused)")
-            handle_metric_count("failed, 503") 
+            handle_metric_count("failed_503") 
             return service.prepare_response(503)
 
 
@@ -34,7 +34,7 @@ def health_check():
 @app.route('/healthz', methods=['POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'])
 def handle_methods():
     logger.error("Method Not Allowed (Only GET method is allowed)")
-    handle_metric_count("failed, 405")
+    handle_metric_count("failed_405")
     return service.prepare_response(405)
 
 
@@ -42,7 +42,7 @@ def handle_methods():
 @app.errorhandler(404)
 def showMessage(error=None):
     logger.error("Not Found (Please check the URL)")
-    handle_metric_count("failed, 404")
+    handle_metric_count("failed_404")
     return service.prepare_response(404)
 
 
@@ -50,7 +50,7 @@ def showMessage(error=None):
 @app.errorhandler(500)
 def showMessage(error=None):
     logger.error("Service Unavailable (Unexpected error)")
-    handle_metric_count("failed, 503")
+    handle_metric_count("failed_503")
     return service.prepare_response(503)
 
 
@@ -64,11 +64,11 @@ def handle_create_assignment():
         logger.debug(auth)
         if not auth:
             logger.error("Unauthorised (Enter credentials)")
-            handle_metric_count("failed, 401")
+            handle_metric_count("failed_401")
             return Response('Unauthorized', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
     except ValueError:
         logger.error("Invalid Authorization header format")
-        handle_metric_count("failed, 400")
+        handle_metric_count("failed_400")
         return service.prepare_response(400)
 
     logger.info("Inside Create Assignment Method")
@@ -80,7 +80,7 @@ def handle_create_assignment():
 
         if not service.validate_mandatory_fields(request_data, mandatory_fields):
             logger.error("Mandatory fields missing")
-            handle_metric_count("failed, 400")
+            handle_metric_count("failed_400")
             return service.prepare_response(400)
 
         assignment_data = service.create_assignment(
@@ -88,15 +88,15 @@ def handle_create_assignment():
 
         if (assignment_data):
             logger.info("Assignment Created Successfully")
-            handle_metric_count("success, 201")
+            handle_metric_count("success_201")
             return service.prepare_assignments_response(201, assignment_data)
         else:
             logger.error("Bad Request, check values for points and num_of_attempts fields")
-            handle_metric_count("failed, 400")
+            handle_metric_count("failed_400")
             return service.prepare_response(400)
     else:
         logger.error("Unauthorised, check credentials")
-        handle_metric_count("failed, 401")
+        handle_metric_count("failed_401")
         return service.prepare_response(401)
 
 
@@ -108,11 +108,11 @@ def handle_update_assignment(assignment_id):
         auth = request.authorization
         if not auth:
             logger.error("Unauthorised, enter Auth values")
-            handle_metric_count("failed, 401")
+            handle_metric_count("failed_401")
             return Response('Unauthorized', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
     except ValueError:
         logger.error("Invalid Authorization header format")
-        handle_metric_count("failed, 400")
+        handle_metric_count("failed_400")
         return service.prepare_response(400)
 
     logger.info("Inside Update Assignment Method")
@@ -127,30 +127,30 @@ def handle_update_assignment(assignment_id):
 
             if not service.validate_mandatory_fields(request_data, mandatory_fields):
                 logger.error("Bad Request, Mandatory Fields Missing")
-                handle_metric_count("failed, 400")
+                handle_metric_count("failed_400")
                 return service.prepare_response(400)
 
             assignment_data = service.update_assignment(
                 assignment_id, request.get_json())
             if assignment_data:
                 logger.info("Assignment Updated Successfully")
-                handle_metric_count("success, 204")
+                handle_metric_count("success_204")
                 return service.prepare_response(204)
             else:
                 logger.error("Bad Request, check values for points and num_of_attempts fields")
-                handle_metric_count("failed, 400")
+                handle_metric_count("failed_400")
                 return service.prepare_response(400)
         elif (service.check_owner(assignment_id, auth) == "No Match"):
             logger.error("Forbidden, check credentials")
-            handle_metric_count("failed, 403")
+            handle_metric_count("failed_403")
             return service.prepare_response(403)
         else:
             logger.error("Assignment not found for update")
-            handle_metric_count("failed, 404")
+            handle_metric_count("failed_404")
             return service.prepare_response(404)
     else:
         logger.error("Unauthorised, check credentials")
-        handle_metric_count("failed, 401")
+        handle_metric_count("failed_401")
         return service.prepare_response(401)
 
 
@@ -162,11 +162,11 @@ def handle_delete_assignment(assignment_id):
         auth = request.authorization
         if not auth:
             logger.error("Unauthorised, enter Auth values")
-            handle_metric_count("failed, 401")
+            handle_metric_count("failed_401")
             return Response('Unauthorized', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
     except ValueError:
         logger.error("Invalid Authorization header format")
-        handle_metric_count("failed, 400")
+        handle_metric_count("failed_400")
         return service.prepare_response(400)
 
     logger.info("Inside Delete Assignment Method")
@@ -176,19 +176,19 @@ def handle_delete_assignment(assignment_id):
         if (service.check_owner(assignment_id, auth) == "Match"):
             if (service.delete_assignment(assignment_id)):
                 logger.info("Assignment Deleted Successfully")
-                handle_metric_count("success, 204")
+                handle_metric_count("success_204")
                 return service.prepare_response(204)
         elif (service.check_owner(assignment_id, auth) == "No Match"):
             logger.error("Forbidden, check credentials")
-            handle_metric_count("failed, 403")
+            handle_metric_count("failed_403")
             return service.prepare_response(403)
         else:
             logger.error("Assignment not found for deletion")
-            handle_metric_count("failed, 404")
+            handle_metric_count("failed_404")
             return service.prepare_response(404)
     else:
         logger.error("Unauthorised, check credentials")
-        handle_metric_count("failed, 401")
+        handle_metric_count("failed_401")
         return service.prepare_response(401)
 
 
@@ -200,11 +200,11 @@ def handle_get_all_assignments():
         auth = request.authorization
         if not auth:
             logger.error("Unauthorised, enter Auth values")
-            handle_metric_count("failed, 401")
+            handle_metric_count("failed_401")
             return Response('Unauthorized', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
     except ValueError:
             logger.error("Invalid Authorization header format")
-            handle_metric_count("failed, 400")
+            handle_metric_count("failed_400")
             return service.prepare_response(400)
 
     logger.info("Inside Get All Assignment Method")
@@ -212,7 +212,7 @@ def handle_get_all_assignments():
 
     if (request.data or request.args):
         logger.error("Method Not Allowed (Request body, query params or path params not allowed)")
-        handle_metric_count("failed, 400")
+        handle_metric_count("failed_400")
         return service.prepare_response(400)
     else:
 
@@ -220,11 +220,11 @@ def handle_get_all_assignments():
             # print("inside get all assignments method")
             assignment_list = service.get_all_assignment()
             logger.info("Success All Assignments retrieved")
-            handle_metric_count("success, 200")
+            handle_metric_count("success_200")
             return service.prepare_assignments_response(200, assignment_list)
         else:
             logger.error("Unauthorised, check credentials")
-            handle_metric_count("failed, 401")
+            handle_metric_count("failed_401")
             return service.prepare_response(401)
 
 
@@ -235,11 +235,11 @@ def handle_get_by_id_assignment(assignment_id):
         auth = request.authorization
         if not auth:
             logger.error("Unauthorised, enter Auth values")
-            handle_metric_count("failed, 401")
+            handle_metric_count("failed_401")
             return Response('Unauthorized', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
     except ValueError:
         logger.error("Invalid Authorization header format")
-        handle_metric_count("failed, 400")
+        handle_metric_count("failed_400")
         return service.prepare_response(400)
     
     logger.info("Inside Get Assignment by ID Method")
@@ -249,19 +249,19 @@ def handle_get_by_id_assignment(assignment_id):
         if (service.check_owner(assignment_id, auth) == "Match"):
             assignment_data = service.get_assignment_by_id(assignment_id)
             logger.info("Assignment retrieved")
-            handle_metric_count("success, 200")
+            handle_metric_count("success_200")
             return service.prepare_assignments_response(200, assignment_data)
         elif (service.check_owner(assignment_id, auth) == "No Match"):
             logger.error("Forbidden, check credentials")
-            handle_metric_count("failed, 403")
+            handle_metric_count("failed_403")
             return service.prepare_response(403)
         else:
             logger.error("Assignnment not found")
-            handle_metric_count("failed, 404")
+            handle_metric_count("failed_404")
             return service.prepare_response(404)
     else:
         logger.error("Unauthorised, check credentials")
-        handle_metric_count("failed, 401")
+        handle_metric_count("failed_401")
         return service.prepare_response(401)
 
 
--- statsd_config.py
+++ statsd_config.py
@@ -6,7 +6,6 @@ statsd_client = statsd.StatsClient('localhost', 8125)
 
 
 def handle_metric_count(status):
-    metric_name = "API:" + request.path + "_Method:" + request.method + "_" + status
+    metric_name = f'{request.path}_{request.method}_{status}'
     logger.info(f"metric incremented {metric_name}")
-    print(metric_name)
-    statsd_client.incr(metric_name) 
+    statsd_client.incr(metric_name)
