ccef63a820bb71abde6370fd310369d40fca6b9f
Aditya Prakash <prakash.adi@northeastern.edu>
2023-11-08 11:16:32 -0500

    Update statsd_config.py

--- statsd_config.py
+++ statsd_config.py
@@ -1,9 +1,12 @@
 import statsd
 from flask import request
+from logging_config import logger
+
 statsd_client = statsd.StatsClient('127.0.0.1', 8125)
 
+
 def handle_metric_count(status):
-    print(request.method)
-    print(request.path)
     metric_name = "API: " + request.path + "API Method: " + request.method + status
+    logger.info(f"metric incremented {metric_name}")
+    print(metric_name)
     statsd_client.incr(metric_name, 1) 
